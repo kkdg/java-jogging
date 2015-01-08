3cbab885f361252a98964e07699e4348763f6f6e
Kwag De Gi <tech@skyaperture.com>
2015-01-09 08:39:30 +0900

    test 0109

---
+++ 0109/0109.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author dex
+ *
+ */
+public class 0109 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi World!!!");
+
+	}
+
+}
