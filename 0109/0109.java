af7265aafd075e6a934a9ba284de5c71310a423b
Kwag De Gi <code@skyaperture.com>
2015-01-09 08:44:03 +0900

    project mayhem

--- 0109/0109.java
+++ 0109/0109.java
@@ -15,7 +15,7 @@ public class 0109 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi World!!!");
-
+		System.out.println("Command Line Program Imported to Eclipse!!")
 	}
 
 }
