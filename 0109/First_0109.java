6e8e1dcaf06f5ac5e5f0feaaf0c43d585cf3f71f
Kwag De Gi <code@skyaperture.com>
2015-01-09 09:32:21 +0900

    enum gone wild

--- 0109/First_0109.java
+++ 0109/First_0109.java
@@ -11,6 +11,14 @@ public class First_0109 {
 		// TODO Auto-generated method stub
 		System.out.println("Hi World!!!");
 		System.out.println("Command Line Program Imported to Eclipse!!");
+		FreshJuice juice = new FreshJuice();
+		juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+		System.out.println("Size : " + juice.size);
 	}
 
+}
+
+class FreshJuice {
+	enum FreshJuiceSize { SMALL, MEDIUM, LARGE }
+	FreshJuiceSize size;
 }
