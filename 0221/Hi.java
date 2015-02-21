f207981cf3f05191d9be3fd57be130865aa2bf41
Kwag De Gi <code@skyaperture.com>
2015-02-21 13:41:29 +0900

    if

--- 0221/Hi.java
+++ 0221/Hi.java
@@ -2,5 +2,11 @@ class Hi {
 
 	public static void main(String args[]) {
 		System.out.println("Hi,..");
+
+		int x = 10;
+
+		if ( x < 20 ) {
+			System.out.print("This is if statement");
+		}
 	}
 }
