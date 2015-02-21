1835420d33225e351a40cb885241be7c42b2ec58
Kwag De Gi <code@skyaperture.com>
2015-02-21 14:04:06 +0900

    method calc

---
+++ 0221/Method.java
@@ -0,0 +1,16 @@
+
+class Method {
+	public static void main(String[] param) {
+		int a = 10;
+		int b = 20;
+		int s;
+		s = funcName(a, b);
+		System.out.println(s);
+	}
+
+	public static int funcName(int a, int b) {
+
+		return a + b;
+	}
+
+}
